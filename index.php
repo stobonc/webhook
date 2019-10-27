734f5d342a881e09574b154bc7dd5f01d8df92c7
stobonc <39226631+stobonc@users.noreply.github.com>
2019-10-27 09:58:34 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,44 +1,45 @@
 <?php
-echo 'hola';
-
-$method=$_SERVER['REQUEST_METHOD'];
-
-if($method=="POST"){
-
-$requestBody=file_get_contents('php://input');
-$json=json_decode($requestBody);
-$text=$json->queryText->parameters->user;
-
-switch ($text) {
-    case 'stobon@stobon.com':
-        $speech='como estas stobon@';
-        break;
-
-     case 'bye':
-        $speech='gracias por visitar bye';
-        break;
-    
-    case 'ayuda':
-        $speech='si te puedo ayudar';
-        break;
-
-    default:
-    $speech='fue un placer';
-        break;
+function processMessage($update) {
+    //el action getcontent que hemos indicado en el intent (action and parameters)
+    if($update["queryResult"]["action"] == "getcontent"){
+        //parametros creados en el intent. En nuestro caso vendrá el nombre de la ciudad
+        $params = $update["queryResult"]["parameters"];
+        //obtenemos el nombre de la ciudad
+        $city = $params["geo-city"];
+        
+        //obtenemos la temperatura
+        $temperatura = getTemperatura($city);
+        
+        //creamos el mensaje a mostrar al usuario
+        sendMessage(array(
+            "fulfillmentText" => "La temperatura para hoy en ".$city." es de ".$temperatura." grados c",
+            "source"=> "javamovil.info"
+        ));
+    }else{
+        //mensaje de error
+        sendMessage(array(
+            "fulfillmentText"=> "Se ha producido un error",
+            "source"=> "example.com"
+        ));
+    }
 }
 
-$response=new \stdClass();
-$response->speech="";
-$response->displayTex="";
-$response->source="webhook";
-echo json_encode($response);
-
-
-
-
+//obtenemos la temperatura por medio de la api de openweathermap.org
+function getTemperatura($city){
+    $json_file = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q='.$city.'&APPID=a9784faedcd1ec2ade480136f46a6d4a');
+    $vars = json_decode($json_file);
+    $cond = $vars->main;
+    return $temp_c = (int)($cond->temp - 273.15);
 }
-else{
+ 
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
 
-echo"metodo no accesado";
+//obtenemos el post desde dialogflow
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if(isset($update["queryResult"]["action"])) {
+    processMessage($update);
 }
 ?>
